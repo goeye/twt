0a7767059a59263c5f8f31357ea71df75a915053
goeye <just_for_github@outlook.com>
2026-04-12 22:06:51 +0800

    Fix docs heading anchors to show hash on hover

--- apps/docs/docs/.vuepress/.temp/pages/index.html.js
+++ apps/docs/docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/pg133/Downloads/TWT/TWT/twt/apps/docs/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"TWT Chat 开发者文档\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"TWT Chat 开发者文档\"},\"headers\":[],\"git\":{\"updatedTime\":1775970964000},\"filePathRelative\":\"index.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"TWT Chat 开发者文档\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"TWT Chat 开发者文档\"},\"headers\":[],\"git\":{\"updatedTime\":1776000882000},\"filePathRelative\":\"index.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- apps/docs/docs/.vuepress/.temp/pages/widget/index.html.js
+++ apps/docs/docs/.vuepress/.temp/pages/widget/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/pg133/Downloads/TWT/TWT/twt/apps/docs/docs/.vuepress/.temp/pages/widget/index.html.vue"
-const data = JSON.parse("{\"path\":\"/widget/\",\"title\":\"网站小部件\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"功能特性\",\"slug\":\"功能特性\",\"link\":\"#功能特性\",\"children\":[]},{\"level\":2,\"title\":\"文档目录\",\"slug\":\"文档目录\",\"link\":\"#文档目录\",\"children\":[]}],\"git\":{\"updatedTime\":1775970964000},\"filePathRelative\":\"widget/README.md\"}")
+const data = JSON.parse("{\"path\":\"/widget/\",\"title\":\"网站小部件\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"功能特性\",\"slug\":\"功能特性\",\"link\":\"#功能特性\",\"children\":[]},{\"level\":2,\"title\":\"文档目录\",\"slug\":\"文档目录\",\"link\":\"#文档目录\",\"children\":[]}],\"git\":{\"updatedTime\":1776000882000},\"filePathRelative\":\"widget/README.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
